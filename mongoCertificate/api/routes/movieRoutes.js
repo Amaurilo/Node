0acb693879560bc59602ca06c76e780f95f52c7e
Amaurilo <amaurilo@outlook.com>
2018-10-07 17:32:37 -0300

    atualizacao

--- mongoCertificate/api/routes/movieRoutes.js
+++ mongoCertificate/api/routes/movieRoutes.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 const router = express.Router();
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+
 
 const movieDetails = require('../../models/movieSchema');
 const mongoose = require ('mongoose');
@@ -12,7 +17,7 @@ router.get('/',(req, res, next) => {
     //captura querystring
     const year = req.query.ano;
     const rating = req.query.rating;
-    if(year !=null)
+    if(year !=null && rating == null)
     {
         movieDetails.find(
             {year: {$gte: parseInt(year)}}
@@ -25,19 +30,34 @@ router.get('/',(req, res, next) => {
             res.status(500).json({error: err});   
         });  
     }
-    // fim captura querystring
-    
-    /*
-    movieDetails.find()
-    .exec()
-    .then(doc => {
-        res.status(200).json(doc);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({error: err});   
-    });  
-    */
+    if(year !=null && rating != null)
+    {
+        movieDetails.find(
+            {year: {$gte: parseInt(year)},
+            'imdb.rating': {$gte: parseInt(rating)}
+        }
+        ).exec()
+        .then(doc => {
+            res.status(200).json(doc);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({error: err});   
+        });  
+    }
+    if (year ==null && rating == null)
+    {
+        movieDetails.find()
+        .exec()
+        .then(doc => {
+            res.status(200).json(doc);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({error: err});   
+        });  
+    }
+
 });
 
 router.get('/:director', (req, res, next) => {
@@ -47,7 +67,7 @@ router.get('/:director', (req, res, next) => {
     })
     .exec()
     .then(doc1 => {
-        res.status(202).json(doc1);
+        res.status(200).json(doc1);
     })
     .catch(err => {
         console.log(err);
@@ -57,7 +77,39 @@ router.get('/:director', (req, res, next) => {
 });
 
 router.put('/', (req, res, next) => {
-    res.status(200).json({resultado: 'ok'});
+    const putBody = req.body;
+
+    const Todo = require("../../models/movieSchema");
+
+    const newTodoObj = new Todo(req.body);
+    console.log(newTodoObj);
+    newTodoObj.save(err => {
+        if (err) return res.status(500).send(err);
+        return res.status(200).send(newTodoObj);
+    });
+
+
+   // movieDetails.insert(putBody);
+    
+   /*
+   res.status(200).json({
+            resultado:  body
+        });
+        */
 });
+
+router.post('/', (req, res) => {
+    //var movie = mongoose.model("User", movieSchema);
+    var myData = new movieDetails(req.body);
+    myData.save()
+    .then(item => {
+    //res.send("item saved to database");
+    res.send(myData);
+    })
+    .catch(err => {
+    res.status(400).send("unable to save to database");
+    });
+   });
+
 
 module.exports = router;
--- mongoCertificate/models/movieSchema.js
+++ mongoCertificate/models/movieSchema.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const movieSchema = mongoose.Schema({
     title: String, 
-    plot: String
+    director: String,
+    year: Number
 });
 
 module.exports = mongoose.model('movieDetails',movieSchema,'movieDetails')
